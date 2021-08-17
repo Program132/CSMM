5c9a866874381ea0e1ea08b0afee5045722f82a8
Program <67320703+Program132@users.noreply.github.com>
2021-08-17 17:21:48 +0200

    Ajout des outils.

--- src/main/java/fr/program/csmm/init/ModItems.java
+++ src/main/java/fr/program/csmm/init/ModItems.java
@@ -1,12 +1,11 @@
 package fr.program.csmm.init;
 
 import fr.program.csmm.CSMM;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import fr.program.csmm.utils.CustomItemTiers;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ForgeRegistry;
 
 public class ModItems {
 
@@ -14,5 +13,11 @@ public class ModItems {
 
     public static final RegistryObject<Item> COPPER_INGOT = ITEMS.register("copper_ingot", () -> new Item(new Item.Properties().maxStackSize(64).group(ItemGroup.MATERIALS)));
     public static final RegistryObject<Item> COPPER_STICK = ITEMS.register("copper_stick", () -> new Item(new Item.Properties().maxStackSize(1).group(ItemGroup.MATERIALS))); // 1 baton max
+
+
+    public static final RegistryObject<Item> COPPER_SWORD = ITEMS.register("copper_sword", () -> new SwordItem(CustomItemTiers.COPPER, 3, -2.4f, new Item.Properties().maxStackSize(1).group(ItemGroup.TOOLS)));
+    public static final RegistryObject<Item> COPPER_PICKAXE = ITEMS.register("copper_pickaxe", () -> new PickaxeItem(CustomItemTiers.COPPER, 0, -2.4f, new Item.Properties().maxStackSize(1).group(ItemGroup.TOOLS)));
+    public static final RegistryObject<Item> COPPER_AXE = ITEMS.register("copper_axe", () -> new AxeItem(CustomItemTiers.COPPER, 0, -2.4f, new Item.Properties().maxStackSize(1).group(ItemGroup.TOOLS)));
+    public static final RegistryObject<Item> COPPER_SHOVEL = ITEMS.register("copper_shovel", () -> new ShovelItem(CustomItemTiers.COPPER, 0, -2.4f, new Item.Properties().maxStackSize(1).group(ItemGroup.TOOLS)));
 
 }
