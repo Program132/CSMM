cfea0863ab0b912dcca8edf90b7e061757149aa0
Program <67320703+Program132@users.noreply.github.com>
2021-08-18 19:43:02 +0200

    Added more apples

--- src/main/java/fr/program/csmm/init/ModItems.java
+++ src/main/java/fr/program/csmm/init/ModItems.java
@@ -1,9 +1,14 @@
 package fr.program.csmm.init;
 
 import fr.program.csmm.CSMM;
+import fr.program.csmm.utils.CustomArmorMaterials;
 import fr.program.csmm.utils.CustomItemTiers;
 import fr.program.csmm.utils.ModItemGroups;
+import javafx.scene.effect.Effect;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,5 +27,19 @@ public class ModItems {
 
 
     public static final RegistryObject<Item> AMETHYST_GEM = ITEMS.register("amethyst_gem", () -> new Item(new Item.Properties().tab(ModItemGroups.CSMM_TAB)));
+
+    public static final RegistryObject<Item> AMETHYST_HELEMT = ITEMS.register("amethyst_helmet", () -> new ArmorItem(CustomArmorMaterials.AMETHYST_ARMOR, EquipmentSlotType.HEAD, new Item.Properties().tab(ItemGroup.TAB_COMBAT)));
+    public static final RegistryObject<Item> AMETHYST_CHESTPLATE = ITEMS.register("amethyst_chestplate", () -> new ArmorItem(CustomArmorMaterials.AMETHYST_ARMOR, EquipmentSlotType.CHEST, new Item.Properties().tab(ItemGroup.TAB_COMBAT)));
+    public static final RegistryObject<Item> AMETHYST_LEGGINGS = ITEMS.register("amethyst_leggings", () -> new ArmorItem(CustomArmorMaterials.AMETHYST_ARMOR, EquipmentSlotType.LEGS, new Item.Properties().tab(ItemGroup.TAB_COMBAT)));
+    public static final RegistryObject<Item> AMETHYST_BOOTS = ITEMS.register("amethyst_boots", () -> new ArmorItem(CustomArmorMaterials.AMETHYST_ARMOR, EquipmentSlotType.FEET, new Item.Properties().tab(ItemGroup.TAB_COMBAT)));
+
+
+    public static final RegistryObject<Item> BLUE_APPLE = ITEMS.register("blue_apple", () -> new Item(new Item.Properties()
+            .tab(ItemGroup.TAB_FOOD)
+            .food(new Food.Builder().nutrition(7).saturationMod(0.8f).meat().effect(() -> new EffectInstance(Effects.MOVEMENT_SPEED, 20*30, 1), 1.0f).build())));
+
+    public static final RegistryObject<Item> GREEN_APPLE = ITEMS.register("green_apple", () -> new Item(new Item.Properties()
+            .tab(ItemGroup.TAB_FOOD)
+            .food(new Food.Builder().nutrition(10).alwaysEat().saturationMod(0.8f).effect(() -> new EffectInstance(Effects.DAMAGE_BOOST, 20*15, 1), 0.5f).build())));
 
 }
